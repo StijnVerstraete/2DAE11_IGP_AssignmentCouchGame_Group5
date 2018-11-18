acf4c3393fe17937048998792cc243da94814a03
K_ar_el <verhulstkarel@hotmail.be>
2018-11-18 15:49:47 +0100

    Input Manager
    edit input manager to play with more than 1 xbox controller
    Must be tested with controllers

---
+++ Assets/Scripts/XboxControllers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XboxControllers : MonoBehaviour {
+
+    [SerializeField]
+    private string _horizontalInputPlayer;
+    [SerializeField]
+    private string _verticalInputPlayer;
+    [SerializeField]
+    private string _aButtonInputPlayer;
+
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log(_horizontalInputPlayer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis(_horizontalInputPlayer);
+        float v = Input.GetAxis(_verticalInputPlayer);
+
+        this.transform.Translate(new Vector3(h,0,v) * Time.deltaTime);
+        
+
+        if (Input.GetButtonDown(_aButtonInputPlayer))
+        {
+            Debug.Log("A button is down");
+        }
+    }
+}
