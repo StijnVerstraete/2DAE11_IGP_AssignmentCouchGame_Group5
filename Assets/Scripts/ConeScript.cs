8abec5a6ca4591442273040591de7bc5c8046f9a
StijnVerstraete <43540802+StijnVerstraete@users.noreply.github.com>
2018-11-24 15:56:08 +0100

    Implemented Cone Asset + Added Controller Rumble
    Rumble needs testing with multiple controllers

---
+++ Assets/Scripts/ConeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeScript : MonoBehaviour {
+
+    private Rigidbody _rb;
+    [SerializeField] private GameObject _car;
+	void Start ()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _car = GameObject.FindGameObjectWithTag("Car");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        _rb.AddForce(Vector3.Scale(_car.transform.forward, new Vector3(0, 30, 0)));
+    }
+}
---
+++ Assets/Scripts/ControllerRumble.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class ControllerRumble : MonoBehaviour {
+
+    private float _rumbleLength = 0.2f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //stop rumble after short burst
+        if (_rumbleLength > 0)
+        {
+            _rumbleLength -= Time.fixedDeltaTime;
+        }
+        if (_rumbleLength <=0)
+        {
+            _rumbleLength = 0;
+            GamePad.SetVibration(PlayerIndex.One, 0f, 0f);
+            GamePad.SetVibration(PlayerIndex.Two, 0f, 0f);
+            GamePad.SetVibration(PlayerIndex.Three, 0f, 0f);
+            GamePad.SetVibration(PlayerIndex.Four, 0f, 0f);
+        }
+        Debug.Log(_rumbleLength);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //rumble
+        if (collision.gameObject.tag == "Rumble")
+        {
+            GamePad.SetVibration(PlayerIndex.One, 5f, 5f);
+            GamePad.SetVibration(PlayerIndex.Two, 5f, 5f);
+            GamePad.SetVibration(PlayerIndex.Three, 5f, 5f);
+            GamePad.SetVibration(PlayerIndex.Four, 5f, 5f);
+            _rumbleLength = 0.2f;
+        }
+    }
+}
