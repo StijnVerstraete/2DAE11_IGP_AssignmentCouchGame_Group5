1d1c108a071b23649a755ebb21aaa9fa018b4acf
K_ar_el <verhulstkarel@hotmail.be>
2018-11-25 21:54:42 +0100

    add some scripting to menu
    button react with menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+    [SerializeField]
+    private List<int> _assignedControllers = new List<int>();
+
+    private PlayerPanel[] _panels;
+    
+
+	// Use this for initialization
+	void Start () {
+        _panels = FindObjectsOfType<PlayerPanel>().OrderBy(t=> t.PlayerNumber).ToArray();  
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // get the length of the controller connected to this pc
+
+        for (int i = 1; i < Input.GetJoystickNames().Length + 1; i++)
+        {
+            if (_assignedControllers.Contains(i))
+                continue;
+
+            if (Input.GetButtonDown("A" + i + "_XboxButton"))
+            {
+                AddPlayerController(i);
+                Debug.Log("A" + i + "_XboxButton");
+                break;
+            }
+        }
+	}
+
+    private void AddPlayerController(int controller)
+    {
+        _assignedControllers.Add(controller);
+
+        //for (int i = 0; i < _panels.Length; i++)
+        //{
+        //    if (!_panels[i].HasControllerAssigned)
+        //    {
+        //        _panels[i].UpdatePanels(controller);
+        //    }
+        //}
+
+        if (!_panels[controller - 1].HasControllerAssigned)
+        {
+            _panels[controller - 1].UpdatePanels(controller);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerPanel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPanel : MonoBehaviour {
+
+    [SerializeField]
+    private Image _imgButton;
+    [SerializeField]
+    private Text _txtJoin;
+    [SerializeField]
+    private Text _txtAssigned;
+
+    public int PlayerNumber;
+
+    public bool HasControllerAssigned { get; set; }
+    
+
+    public void UpdatePanels(int controller)
+    {
+        _imgButton.enabled = false;
+        _txtJoin.enabled = false;
+        _txtAssigned.enabled = true;
+        _txtAssigned.text = "Controller " + controller + " has joined";
+
+        HasControllerAssigned = true;
+    }
+}
