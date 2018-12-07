24f7d1bf886bbff8f35e1ab751e5560c9ab02b8d
StijnVerstraete <43540802+StijnVerstraete@users.noreply.github.com>
2018-12-07 22:25:58 +0100

    Graphics Tweaks (toon shader/exhaust particles), UI font, Tutorial System
    Imported standard assets for shader, its why so many changes woops

---
+++ Assets/Scripts/TutorialMessage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialMessage : MonoBehaviour {
+
+    [SerializeField] private string _instructionText;
+
+    [SerializeField] private Text _textObject;
+    [SerializeField] private GameObject _panel;
+
+    public bool ShowText = false;
+
+    private void Update()
+    {
+        if (ShowText == true && (_textObject.gameObject.activeSelf == false || _panel.activeSelf == false))
+        {
+            _textObject.gameObject.SetActive(true);
+            _panel.SetActive(true);
+
+            _textObject.text = _instructionText;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerCar")
+        {
+            ShowText = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "TutorialText")
+        {
+            _textObject.gameObject.SetActive(false);
+            _textObject.gameObject.SetActive(false);
+            ShowText = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/TutorialMessageActivation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMessageActivation : MonoBehaviour {
+
+    [SerializeField] private TutorialMessage _currentMessage;
+	void Start () {
+		
+	}
+
+
+}
