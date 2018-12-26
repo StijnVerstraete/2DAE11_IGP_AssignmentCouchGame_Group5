a7d318c68548e2779843fe80528be17902693637
StijnVerstraete <43540802+StijnVerstraete@users.noreply.github.com>
2018-12-26 19:34:46 +0100

    Added stereosound in splitscreen

--- Assets/Scripts/AudioHandler.cs
+++ Assets/Scripts/AudioHandler.cs
@@ -10,12 +10,34 @@ public class AudioHandler : MonoBehaviour {
 
     [SerializeField] AudioClip[] _carClips; //0 = idle 1 = accelrate 2 = brake
     private int _clipPlaying;
-	void Start ()
+
+    private int _stereoChannel = 0;
+
+    [SerializeField] private GameMode _gameMode;
+
+    [SerializeField] private int _sourceID;
+
+    private void Start()
     {
+        _gameMode = (GameMode)System.Enum.Parse(typeof(GameMode), PlayerPrefs.GetString("GameMode", "Teams"));
+        _carControls = GetComponentInParent<CarControls>();
+        if (_gameMode == GameMode.Teams)
+        {
+            if (_sourceID == 0)
+            {
+                _stereoChannel = -1;
+            }
+            if (_sourceID == 1)
+            {
+                _stereoChannel = 1;
+            }
+        }
+        else
+        {
+            _stereoChannel = 0;
+        }
     }
-	
-	// Update is called once per frame
-	void Update ()
+    void Update ()
     {
         CarAudio();
 
@@ -29,12 +51,14 @@ public class AudioHandler : MonoBehaviour {
                 _audioSource.clip = _carClips[0];
                 _clipPlaying = _carControls.AccelerationLevel;
                 _audioSource.volume = 0.5f;
+                _audioSource.panStereo = _stereoChannel;
             }
             if (_carControls.AccelerationLevel > 0)
             {
                 _audioSource.clip = _carClips[1];
                 _clipPlaying = _carControls.AccelerationLevel;
                 _audioSource.volume = 0.5f;
+                _audioSource.panStereo = _stereoChannel;
                 if (_carControls.AccelerationLevel == 2)
                 {
                     _audioSource.volume = 0.8f;
@@ -45,6 +69,7 @@ public class AudioHandler : MonoBehaviour {
                 _audioSource.clip = _carClips[2];
                 _clipPlaying = _carControls.AccelerationLevel;
                 _audioSource.volume = 0.5f;
+                _audioSource.panStereo = _stereoChannel;
                 if (_carControls.AccelerationLevel == -2)
                 {
                     _audioSource.volume = 0.8f;
